7985721a37bbbcd58c658898dc16fda4876cd08a
DagazProject <dagaz-project@mail.ru>
2023-09-07 10:54:54 +0300

    map

---
+++ index-map.pl
@@ -0,0 +1,38 @@
+print "<!DOCTYPE html>\n";
+print "<html>\n";
+print "  <head>\n";
+print "    <META http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n";
+print "    <META name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>\n";
+print "    <link rel=\"stylesheet\" href=\"css/map.css\"/>\n";
+print "    <link rel=\"stylesheet\" href=\"css/bootstrap.min.css\"/>\n";
+print "    <title>Dagaz Release 1.4.0</title>\n";
+print "  </head>\n";
+print "  <body>\n";
+print "    <div class=\"divGameIconWrap\">\n";
+
+while (<>) {
+  if (/^[^;]+;([^;]+);([^;]+);([^;]*);([^;]+)/) {
+      my $name = $1;
+      my $href = $2;
+      my $rule = $3;
+      my $view = $4;
+      print "      <div class=\"divGameIcon\">\n";
+      print "        <a href=\"$href\">\n";
+      print "          <img src=\"upload/$view.png\">\n";
+      print "        </a>\n";
+      print "        <p>\n";
+      if ($rule) {
+          print "          <a target=\"_blank\" href=\"$rule\">\n";
+      }
+      print "            <b>$name</b>\n";
+      if ($rule) {
+          print "          </a>\n";
+      }
+      print "        </p>\n";
+      print "      </div>\n";
+  }
+}
+
+print "    </div>\n";
+print "  </body>\n";
+print "</html>\n";
---
+++ index-sql.pl
@@ -0,0 +1,13 @@
+while (<>) {
+  if (/^([^;]+);([^;]+);([^;]+);([^;]*);([^;]+);(.*)/) {
+      my $sect = $1;
+      my $name = $2;
+      my $href = $3;
+      my $rule = $4;
+      my $thmb = $5;
+      my $auth = $5;
+      $name =~ s/\'/\'\'/g;
+      $rule =~ s/\'/\'\'/g;
+      print "insert into tmp(sect, name, href, rule, thmb, auth) values('$sect', '$name', '$href', '$rule', '$thmb', '$auth');\n";
+  }
+}
