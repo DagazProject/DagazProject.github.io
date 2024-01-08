9d36511e63883ba561af1793f36f00742244dd7e
DagazProject <dagaz-project@mail.ru>
2024-01-08 12:12:47 +0300

    Cyclic Checkers (fix)

---
+++ checkers/scripts/cyclic-checkers-fen.js
@@ -0,0 +1,255 @@
+Dagaz.Controller.persistense = "setup";
+
+Dagaz.Model.WIDTH     = 8;
+Dagaz.Model.HEIGHT    = 8;
+
+Dagaz.AI.RESERVE_SIZE = 1;
+
+(function() {
+
+Dagaz.Model.moveToString = function(move) {
+  var r = "";
+  _.each(move.actions, function(a) {
+      if (a[0] === null) return;
+      if (a[1] === null) return;
+      if ((r == "") && (a[0] != null)) {
+          r = r + Dagaz.Model.posToString(a[0][0]);
+      }
+      if ((a[1] !== null) && (a[0][0] != a[1][0])) {
+          r = r + Dagaz.Model.posToString(a[1][0]);
+      }
+  });
+  return r;
+}
+
+var getName = function() {
+  var str = window.location.pathname.toString();
+  var result = str.match(/\/([^.\/]+)\./);
+  if (result) {
+      return result[1].replace("-board", "").replace("-ai", "");
+  } else {
+      return str;
+  }
+}
+
+var badName = function(str) {
+  var result = str.match(/[?&]game=([^&*]*)/);
+  if (result) {
+      return result[1] != getName();
+  } else {
+      return true;
+  }
+}
+
+var getCookie = function() {
+  var result = localStorage.getItem('dagaz.setup');
+  if (result) {
+      if (badName(result)) return "";
+      return result;
+  } else {
+      return "";
+  }
+}
+
+var getSetup = function(setup) {
+  var str = window.location.search.toString();
+  if (setup) {
+      str = setup;
+  }
+  var result = str.match(/[?&]setup=([^&]*)/);
+  if (result) {
+      return result[1];
+  } else {
+      str = getCookie();
+      result = str.match(/[?&]setup=([^&]*)/);
+      if (result) {
+          return result[1];
+      } else {
+          return "";
+      }
+  }
+}
+
+var getTurn = function(setup) {
+  var str = window.location.search.toString();
+  if (setup) {
+      str = setup;
+  }
+  var result = str.match(/[?&]turn=(\d+)/);
+  if (result) {
+      return result[1];
+  } else {
+      str = getCookie();
+      result = str.match(/[?&]turn=(\d+)/);
+      if (result) {
+          return result[1];
+      } else {
+          return "";
+      }
+  }
+}
+
+var createPiece = function(design, c) {
+  if (c == 'P') return Dagaz.Model.createPiece(design.getPieceType("Man"), 1);
+  if (c == 'p') return Dagaz.Model.createPiece(design.getPieceType("Man"), 2);
+  if (c == 'K') return Dagaz.Model.createPiece(design.getPieceType("King"), 1);
+  if (c == 'k') return Dagaz.Model.createPiece(design.getPieceType("King"), 2);
+  return null;
+}
+
+var toBoard = function(pos) {
+  return pos;
+}
+
+var toReserve = function(pos) {
+  var y = (pos / (Dagaz.AI.RESERVE_SIZE * 2)) | 0;
+  var x = pos % (Dagaz.AI.RESERVE_SIZE * 2);
+  var r = y * (Dagaz.AI.RESERVE_SIZE * 2) + x;
+  return r + Dagaz.Model.WIDTH * Dagaz.Model.HEIGHT;
+}
+
+Dagaz.Model.setup = function(board, init) {
+  var design = Dagaz.Model.design;
+  var setup  = getSetup(init);
+  var player = 1;
+  if (setup) {
+      board.clear();
+      var chunks = setup.split('-');
+      var setup = chunks[0];
+      var pos = 0; 
+      for (var i = 0; i < setup.length; i++) {
+           var c = setup[i];
+           if (c != '/') {
+               if ((c >= '0') && (c <= '9')) {
+                   pos += +c;
+               } else {
+                   var piece = createPiece(design, c);
+                   board.setPiece(toBoard(pos), piece);
+                   pos++;
+               }
+               if (pos >= Dagaz.Model.WIDTH * Dagaz.Model.HEIGHT) break;
+           }
+      }
+      setup = chunks[1]; pos = 0; 
+      for (var i = 0; i < setup.length; i++) {
+           var c = setup[i];
+           if (c != '/') {
+               if ((c >= '0') && (c <= '9')) {
+                   pos += +c;
+               } else {
+                   var piece = createPiece(design, c);
+                   board.setPiece(toReserve(pos), piece);
+                   pos++;
+               }
+               if (pos >= (Dagaz.AI.RESERVE_SIZE * 2) * Dagaz.Model.HEIGHT) break;
+           }
+      }
+      var turn = getTurn(init);
+      if (turn) {
+          board.turn   = +turn;
+          board.player = design.currPlayer(board.turn);
+      }
+  }
+}
+
+var getPieceNotation = function(design, piece) {
+  var r = 'X';
+  if (piece.type == design.getPieceType("Man"))  r = 'P';
+  if (piece.type == design.getPieceType("King")) r = 'K';
+  if (piece.player > 1) {
+      return r.toLowerCase();
+  }
+  return r;
+}
+
+Dagaz.Model.getSetup = function(design, board) {
+  var str = "?turn=" + board.turn + ";&setup=";
+  var k = 0; var c = 0;
+  for (var pos = 0; pos < Dagaz.Model.WIDTH * Dagaz.Model.HEIGHT; pos++) {
+       if (k >= Dagaz.Model.WIDTH) {
+           if (c > 0) {
+               str += c;
+           }
+           str += "/";
+           k = 0;
+           c = 0;
+       }
+       k++;
+       if (design.isKilledPos(toBoard(pos))) {
+           if (c > 0) {
+               str += c;
+           }
+           c = 0;
+           str += 'X';
+       } else {
+           var piece = board.getPiece(toBoard(pos));
+           if (piece === null) {
+               if (c > 8) {
+                   str += c;
+                   c = 0;
+               }
+               c++;
+           } else {
+               if (c > 0) {
+                   str += c;
+               }
+               c = 0;
+               str += getPieceNotation(design, piece);
+           }
+       }
+  }
+  if (c > 0) {
+      str += c;
+  }
+  str += '-';
+  k = 0; c = 0;
+  for (var pos = 0; pos < 2 * Dagaz.AI.RESERVE_SIZE * Dagaz.Model.HEIGHT; pos++) {
+       if (k >= (Dagaz.AI.RESERVE_SIZE * 2)) {
+           if (c > 0) {
+               str += c;
+           }
+           str += "/";
+           k = 0;
+           c = 0;
+       }
+       k++;
+       var piece = board.getPiece(toReserve(pos));
+       if (piece === null) {
+           if (c > 8) {
+               str += c;
+               c = 0;
+           }
+           c++;
+       } else {
+           if (c > 0) {
+               str += c;
+           }
+           c = 0;
+           str += getPieceNotation(design, piece);
+       }
+  }
+  if (c > 0) {
+      str += c;
+  }
+  if (board.turn == 0) {
+      str += "-w";
+  } else {
+      str += "-b";
+  }
+  if (Dagaz.Controller.persistense == "setup") {
+      var s = str + "&game=" + getName() + "*";
+      localStorage.setItem('dagaz.setup', s);
+  }
+  return str;
+}
+
+var clearGame = Dagaz.Controller.clearGame;
+
+Dagaz.Controller.clearGame = function() {
+   localStorage.setItem('dagaz.setup', '');
+   if (!_.isUndefined(clearGame)) {
+       clearGame();
+   }
+}
+
+})();
--- checkers/scripts/cyclic-checkers-restrictions.js
+++ checkers/scripts/cyclic-checkers-restrictions.js
@@ -8,10 +8,20 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
-var getPath = function(move) {
-  var r = []; var f = true;
+var getPath = function(move, board) {
+  var r = []; 
+  var f = true; var x = false;
   _.each(move.actions, function(a) {
+      if (x) return;
       if (a[0] === null) return;
+      if (a[1] !== null) return;
+      var piece = board.getPiece(a[0][0]);
+      if ((piece !== null) && (a[1] === null)) {
+          if (piece.player == board.player) {
+              x = true;
+              return;
+          }
+      }
       if (f) {
           if (r.length == 0) r.push(a[0][0]);
           f = false;
@@ -36,10 +46,10 @@ Dagaz.Model.CheckInvariants = function(board) {
   var design = Dagaz.Model.design;
   _.each(board.moves, function(move) {
       if (move.actions.length < 2) return;
-      var p = getPath(move);
+      var p = getPath(move, board);
       _.each(board.moves, function(m) {
           if (m.actions.length < 2) return;
-          var q = getPath(m);
+          var q = getPath(m, board);
           if (isPrefix(q, p)) {
               m.failed = true;
           }
