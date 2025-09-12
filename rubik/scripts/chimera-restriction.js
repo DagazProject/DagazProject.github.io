2d3a37aa5f727db53a4d7bfcd280703d904b1626
DagazProject <dagaz-project@mail.ru>
2025-09-12 10:26:12 +0300

    chimera-restriction (fixed)

--- rubik/scripts/chimera-restriction.js
+++ rubik/scripts/chimera-restriction.js
@@ -8,49 +8,71 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
+var findGroupIx = function(board, move, groups) {
+  var g = []; var r = -1;
+  for (var i = 0; i < move.actions.length; i++) {
+       var a = move.actions[i];
+       if (a[0] === null) continue;
+       var piece = board.getPiece(a[0][0]);
+       if (piece === null) continue;
+       var v = piece.getValue(0);
+       if (v === null) continue;
+       for (var j = 0; j < groups.length; j++) {
+           if (_.indexOf(groups[j], +v) >= 0) {
+               r = j;
+               break;
+           }
+       }
+       if (_.indexOf(g, +v) < 0) g.push(+v);
+  }
+  if (r < 0) {
+     r = groups.length;
+     groups.push(g);
+  } else {
+     groups[r] = _.union(groups[r], g);
+  }
+  return r;
+}
+
+var checkIntersections = function(groups) { 
+  var r = [];
+  if (groups.length == 1) return r;
+  for (var i = 0; i < groups.length; i++) {
+      for (var j = i + 1; j < groups.length; j++) {
+           if (_.intersection(groups[i], groups[j]).length > 0) {
+               r.push(i);
+               r.push(j);
+           }
+      }
+  }
+  return r;
+}
+
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
   var design = Dagaz.Model.design;
-  for (var i = 0; i < board.moves.length; i++) {
-       var move = board.moves[i];
-       if (move.failed) continue;
-       var ids = [];
-       _.each(move.actions, function(a) {
-          if (a[0] === null) return;
-          var pos = a[0][0];
-          var piece = board.getPiece(pos);
-          if (piece === null) return;
-          var v = piece.getValue(0);
-          if (v === null) return;
-          ids.push(+v);
-       });
-       for (var j = 0; j < ids.length; j++) {
-            var id = ids[j];
-            for (var k = 0; k < board.moves.length; k++) {
-                 var m = board.moves[k];
-                 if (k == i) continue;
-                 if (move.mode != m.mode) continue;
-                 var f = false;
-                 _.each(m.actions, function(a) {
-                     if (a[0] === null) return;
-                     var pos = a[0][0];
-                     var piece = board.getPiece(pos);
-                     if (piece === null) return;
-                     var v = piece.getValue(0);
-                     if (v === null) return;
-                     if (v != id) return;
-                     f = true;
-                 });
-                 if (f) {
-                     _.each(m.actions, function(a) {
-                         move.actions.push(a);
-                     });
-                     m.failed = true;
-                 }
+  var all = [];
+  for (var m = 0; m < 6; m++) {
+       var moves  = [];
+       var groups = [];
+       for (var i = 0; i < board.moves.length; i++) {
+            if (board.moves[i].mode != m) continue;
+            var ix = findGroupIx(board, board.moves[i], groups);
+            if (ix >= moves.length) {
+                moves[ix] = Dagaz.Model.createMove(m);
             }
+            _.each(board.moves[i].actions, function(a) {
+                moves[ix].actions.push(a);
+            });
+       }
+       var ixs = checkIntersections(groups);
+       for (var i = 0; i < moves.length; i++) {
+            if (_.indexOf(ixs, i) >= 0) continue;
+            all.push(moves[i]);
        }
   }
+  board.moves = all;
   CheckInvariants(board);
 }
 
