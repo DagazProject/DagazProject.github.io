cdfe3ef440bda024a9cf1836cba4b4f01c1d7090
Valentin Chelnokov <chelnokov.valentin@gmail.com>
2026-02-04 11:18:37 +0300

    Klin Zha (2D)

--- checkmate/scripts/klin-zha-fen.js
+++ checkmate/scripts/klin-zha-fen.js
@@ -10,7 +10,7 @@ var getName = function() {
   var str = window.location.pathname.toString();
   var result = str.match(/\/([^.\/]+)\./);
   if (result) {
-      return result[1].replace("-board", "").replace("-ai", "").replace("-3d", "");
+      return result[1].replace("-board", "").replace("-ai", "").replace("-2d", "");
   } else {
       return str;
   }
