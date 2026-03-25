0a1934379b33894a431e3059ea760f26f26ca3b1
Valentin Chelnokov <chelnokov.valentin@gmail.com>
2026-03-25 08:49:14 +0300

    worker-ai

---
+++ common-scripts/worker-ai.js
@@ -0,0 +1,103 @@
+"use strict";
+
+(function() {
+
+Dagaz.AI.WORKER_NAME = 'scripts/garbochess-worker.js';
+
+let g_backgroundEngine = null;
+let inProgress = false;
+
+function Ai(parent) {
+  this.parent = parent;
+}
+
+const findBot = Dagaz.AI.findBot;
+
+Dagaz.AI.findBot = function(type, params, parent) {
+  if ((type == "external") || (type == "smart") /*|| (type == "1")*/ || (type == "2")) {
+      return new Ai(parent);
+  } else {
+      return findBot(type, params, parent);
+  }
+}
+
+function stop() {
+    if (g_backgroundEngine !== null) {
+        g_backgroundEngine.terminate();
+        g_backgroundEngine = null;
+    }
+    inProgress = false;
+}
+
+function start(fen) {
+    if (g_backgroundEngine !== null) return;
+    g_backgroundEngine = new Worker(Dagaz.AI.WORKER_NAME);
+    g_backgroundEngine.onmessage = onMessage;
+    g_backgroundEngine.error = function (e) {
+       console.log("Error from background worker:" + e.message)
+    }
+    g_backgroundEngine.postMessage("position " + fen.replace('+', ' '));
+    g_backgroundEngine.postMessage("go");
+}
+
+function onMessage(msg) {
+    if (msg.data.match("^pv") == "pv") {
+        console.log(msg.data.substr(3, msg.data.length - 3));
+    } else if (msg.data.match("^message") == "message") {
+        stop();
+        console.log(msg.data.substr(8, msg.data.length - 8));
+    } else {
+        const move = e.data;
+        console.log(move);
+        if (Dagaz.AI.callback) {
+           Dagaz.AI.callback(move);
+        }
+    }
+}
+
+Ai.prototype.setContext = function(ctx, board) {
+  if (this.parent) {
+      this.parent.setContext(ctx, board);
+  }
+  ctx.timestamp = Date.now();
+  ctx.board = board;
+}
+
+Ai.prototype.getMove = function(ctx) {
+  const moves = Dagaz.AI.generate(ctx, ctx.board);
+  if (moves.length == 0) {
+      return { done: true, ai: "nothing" };
+  }
+  if (moves.length == 1) {
+      return {
+           done: true,
+           move: moves[0],
+           time: Date.now() - ctx.timestamp,
+           ai:  "once"
+      };
+  }
+  if (inProgress) {
+      return {
+           done: false,
+           time: Date.now() - ctx.timestamp,
+           ai:  "worker"
+      };
+  }
+  const setup = Dagaz.Model.getSetup(ctx.design, ctx.board);
+  const result = setup.match(/[?&]setup=(.*)/);
+  if (result && (ctx.board.turn >= Dagaz.AI.MIN_TURN)) {
+      inProgress = true;
+      const fen = result[1];
+      start(fen);
+      return {
+           done: false,
+           time: Date.now() - ctx.timestamp,
+           ai:  "worker"
+      };
+  }
+  if (this.parent) {
+      return this.parent.getMove(ctx);
+  }
+}
+
+})();
