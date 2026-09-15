0d086c269734cf3453afdf68842ced18d53a0a70
Valentin Chelnokov <chelnokov.valentin@gmail.com>
2026-09-15 11:46:54 +0300

    amoeba-hole (fixed)

--- checkmate/scripts/amoeba-hole.js
+++ checkmate/scripts/amoeba-hole.js
@@ -19,21 +19,32 @@ Dagaz.Model.CheckInvariants = function(board) {
   for (var pos = 0; pos < 49; pos++) {
        var piece = board.getPiece(pos);
        if (piece === null) continue;
-       if (piece.type != 1) continue;
-       var p = design.navigate(1, pos, 8);
-       if (p === null) continue;
-       if (board.getPiece(p) !== null) continue;
+       if (piece.type != 0) continue;
        _.each([0, 1, 2, 3], function(dir) {
            var p = design.navigate(1, pos, dir);
            if (p === null) return;
            var t = board.getPiece(p);
            if (t !== null) {
-               if (t.type != 0) return;
+               if (t.type == 0) return;
+           }
+           var q = design.navigate(1, p, 8);
+           if (q !== null) {
+               if (board.getPiece(q) !== null) return;
            }
            var m = Dagaz.Model.createMove(1, 10);
-           m.movePiece(pos, p, piece.changeOwner(design.nextPlayer(piece.player)));
+           m.movePiece(pos, p, piece);
            if (t !== null) {
-               m.movePiece(p, pos, t);
+               m.movePiece(p, pos, t.changeOwner(design.nextPlayer(t.player)));
+           }
+           var q = pos;
+           while ((p !== null) && (q !== null)) {
+               p = design.navigate(1, p, 8);
+               q = design.navigate(1, q, 8);
+               if ((p === null) || (q === null)) break;
+               t = board.getPiece(p);
+               if (t !== null) {
+                   m.movePiece(p, q, t);
+               }
            }
            board.moves.push(m);
        });
